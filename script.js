86fda53e6949098d33f4aea8a6d4c7f6cbc6ff6f
Faysal khan <mdfaysalkhancse@gmail.com>
2022-06-17 22:31:25 +0600

    dragstart, dragend and dragover function done

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
 draggables.forEach(draggable => {
+
+    // drag start
     draggable.addEventListener('dragstart', () => {
-        console.log('drag start');
+        draggable.classList.add('.dragging')
+    })
+
+    // drag end
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+
+    containers.forEach(container => {
+        container.addEventListener('dragover', () => {
+            console.log('drag over');
+        })
     })
 })
