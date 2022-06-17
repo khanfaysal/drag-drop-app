50b0508d0bbc574b2f0939710d47d2c981f0549a
Faysal khan <mdfaysalkhancse@gmail.com>
2022-06-17 23:45:25 +0600

    client height measurment with condition

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
+const draggables = document.querySelectorAll('.draggable');
 
 draggables.forEach(draggable => {
 
     // drag start
     draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('.dragging')
+        draggable.classList.add('dragging')
     })
 
     // drag end
@@ -15,8 +15,29 @@ draggables.forEach(draggable => {
     })
 
     containers.forEach(container => {
-        container.addEventListener('dragover', () => {
-            console.log('drag over');
+        container.addEventListener('dragover', e => {
+            e.preventDefault();
+            const afterElement = getDragAfterElement(container, e.clientY)
+            console.log(afterElement);
+            const draggable = document.querySelector('.dragging');
+            container.appendChild(draggable)
         })
     })
 })
+
+// drag after element in y axis
+
+function getDragAfterElement(container, y) {
+
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
